5d15e2ccaeda4b0289ee765cff1ede77cb306076
Erick Gómez <external.erickomar.gomezvalentin@mx.bosch.com>
2020-10-10 09:10:49 -0500

    Se terminó la actividad

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var v int
+	var s []int
+	fmt.Scanln(&v)
+	s = append(s, v)
+	for i := 0; i < s[0]; i++ {
+		fmt.Scanln(&v)
+		s = append(s, v)
+	}
+	out := 0
+	for _, va := range s[1:] {
+		out += va
+	}
+	fmt.Println(out)
 }
